767068a202cdcb95f7be9d33e9d4f1f59fa23cdd
Nikita <mrbushik1@gmail.com>
2022-09-27 10:11:03 +0300

    dysplaying users to the seed

--- src/Components/mainPage.jsx
+++ src/Components/mainPage.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 function MainPage({ outputParams, onChange }) {
   const [users, setUsers] = React.useState([]);
-  const [currentPage, setCurrentPage] = React.useState(20);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [fetching, setFetching] = React.useState(1);
+
   //   const [totalCount, setTotalCount] = React.useState(0);
   // const URL = `https://randomuser.me/api/?nat=${outputParams.nationalities}&results=20`;
   // https://randomuser.me/api/?seed=1.1000&results=20   ----seed URL
@@ -23,25 +24,38 @@ function MainPage({ outputParams, onChange }) {
   }, []);
 
   React.useEffect(() => {
-    fetch(`https://randomuser.me/api/?nat=${outputParams.nationalities}&results=20`, {
-      method: 'GET',
-    })
+    fetch(
+      `https://randomuser.me/api/?nat=${outputParams.nationalities}&results=20${
+        outputParams.seed ? '&seed=1.' + outputParams.seed : ''
+      }`,
+      {
+        method: 'GET',
+      },
+    )
       .then((response) => response.json())
       .then((data) => {
         setUsers([...data.results]);
-        setCurrentPage(currentPage + 10);
       })
       .catch((error) => '');
   }, [outputParams]);
 
   const getUsers = async () => {
-    fetch(`https://randomuser.me/api/?nat=${outputParams.nationalities}&results=20`, {
-      method: 'GET',
-    })
+    let quantityResaults = 20;
+    if (fetching > 1) {
+      quantityResaults = 10;
+    }
+    await fetch(
+      `https://randomuser.me/api/?nat=${outputParams.nationalities}&results=${quantityResaults}${
+        outputParams.seed ? '&seed=1.' + outputParams.seed + '&page=' + currentPage : ''
+      }`,
+      {
+        method: 'GET',
+      },
+    )
       .then((response) => response.json())
       .then((data) => {
         setUsers([...users, ...data.results]);
-        setCurrentPage(currentPage + 10);
+        setCurrentPage(currentPage + 1);
       })
       .catch((error) => '');
   };
